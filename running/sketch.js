d91e30e388026b9b8585756eda334ea84a935a6e
Azuneer <e.gadonnaud@gmail.com>
2022-10-02 20:21:50 +0200

    yay noise

--- running/sketch.js
+++ running/sketch.js
@@ -1,8 +1,43 @@
+// Perlin noise stuff
+// Credits goes to The Coding Train
+
+// var xoff1 = 0;
+// var xoff2 = 10000;
+var inc = 0.01;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(400, 400);
+	pixelDensity(1);
 	// put setup code here
 }
 
 function draw() {
-	// put drawing code here
+	var yoff = 0;
+
+	loadPixels();
+
+	for(var x = 0; x < width; x++) {
+		var xoff = 0;
+		for(var y = 0; y < height; y++){
+			var index = (x+y*width) * 4;
+			var r = noise(xoff,yoff)*255;
+			pixels[index+0] = r;
+			pixels[index+1] = r;
+			pixels[index+2] = r;
+			pixels[index+3] = 255;
+
+			xoff += inc;
+		}
+		yoff += inc
+	}
+	updatePixels();
+	//noLoop();
+
+	// var x = map(noise(xoff1), 0, 1, 0, width)
+	// var y = map(noise(xoff2), 0, 1, 0, height)
+	//
+	// xoff1 += 0.01
+	// xoff2 += 0.01
+
+	// ellipse(x, y, 24, 24);
 }
